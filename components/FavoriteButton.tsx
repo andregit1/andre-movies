7ea6a3b67afb11b1ea887e1f06c981f43d2892e1
andregit1 <andreaswidjaya1@gmail.com>
2023-04-07 13:21:02 +0700

    make favorite button works

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -2,16 +2,64 @@ import axios from "axios"
 import React, {useCallback, useMemo} from "react"
 import useCurrentUser from "@/hooks/useCurrentUser"
 import useFavorites from "@/hooks/useFavorites"
-import { AiOutlinePlus } from "react-icons/ai"
+import { AiOutlinePlus, AiOutlineCheck } from "react-icons/ai"
 
 interface FavoritesButtonProps {
   movieId: string
 }
 
 const FavoritesButton: React.FC<FavoritesButtonProps> = ({movieId}) => {
+
+  const {mutate: mutateFavorites} = useFavorites()
+  const {data: currentUser, mutate } = useCurrentUser()
+
+  const isFavorite = useMemo(() => {
+    const list = currentUser?.favoriteIds || []
+
+    return list.includes(movieId)
+  }, [currentUser, movieId])
+
+  const toggleFavorites = useCallback(async () => {
+    let response
+
+    if (isFavorite) {
+      response = await axios.delete('/api/favorite', {data: {movieId}})
+    } else {
+      response = await axios.post('/api/favorite', {movieId})
+    }
+
+    const updatedFavoriteIds = response?.data?.favoriteIds
+
+    mutate({
+      ...currentUser, favoriteIds: updatedFavoriteIds
+    })
+
+    mutateFavorites()
+  }, [movieId, isFavorite, currentUser, mutate, mutateFavorites])
+
+  const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus
+
   return (
-    <div className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
-      <AiOutlinePlus className="text-white" size={15} />
+    <div
+      onClick={toggleFavorites} 
+      className="
+        cursor-pointer 
+        group/item 
+        w-6 
+        h-6 
+        lg:w-10 
+        lg:h-10 
+        border-white 
+        border-2 
+        rounded-full 
+        flex 
+        justify-center 
+        items-center 
+        transition 
+        hover:border-neutral-300
+      "
+    >
+      <Icon className="text-white" size={15} />
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Navbar from '@/components/Navbar'
 import Billboard from '@/components/Billboard'
 import MovieList from '@/components/MovieList'
 import useMovieList from '@/hooks/useMovieList'
+import useFavorites from '@/hooks/useFavorites'
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context)
@@ -26,6 +27,7 @@ export async function getServerSideProps(context: NextPageContext) {
 export default function Home() {
 
   const {data: movies = [] } = useMovieList()
+  const {data: favorites = [] } = useFavorites()
 
   return (
     <>
@@ -35,6 +37,7 @@ export default function Home() {
       
       <div className="pb-40">
         <MovieList title="Trending now" data={movies} />
+        <MovieList title="My List" data={favorites} />
       </div>
     </>
   )
