a903fdb6a9fbf79cc20838e5e597b87b5c3f919c
andregit1 <andreaswidjaya1@gmail.com>
2023-04-07 15:22:05 +0700

    bugfix wrong api url

--- hooks/useMovie.ts
+++ hooks/useMovie.ts
@@ -2,7 +2,7 @@ import useSWR from "swr"
 import fetcher from "@/lib/fetcher"
 
 const useMovie = (id?: string) => {
-  const { data, error, isLoading, mutate } = useSWR(id ? `/api/movie/${id}` : null , fetcher, {
+  const { data, error, isLoading, mutate } = useSWR(id ? `/api/movies/${id}` : null , fetcher, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
     revalidateOnReconnect: false
